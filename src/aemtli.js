91f84f93ee816267f22a2136c70925541a0f414e
jonarivado <jonathan.dotzler@gmail.com>
2022-01-03 21:22:48 +0100

    added Aemtli feature

---
+++ src/aemtli.js
@@ -0,0 +1,31 @@
+function getAemtli() {
+
+currentdate = new Date();
+let oneJan = new Date(currentdate.getFullYear(),0,1);
+let numberOfDays = Math.floor((currentdate - oneJan) / (24 * 60 * 60 * 1000));
+let result = Math.ceil(( currentdate.getDay() + 1 + numberOfDays) / 7);
+
+let currentMode = result % 3;
+
+if(currentMode === 0) {
+    document.getElementById('name_staubsaugen').innerHTML = "Jonathan";
+    document.getElementById('name_badezimmer').innerHTML = "Tobia";
+    document.getElementById('name_chillen').innerHTML = "Tobias";
+}
+
+if(currentMode === 1) {
+    document.getElementById('name_staubsaugen').innerHTML = "Tobia";
+    document.getElementById('name_badezimmer').innerHTML = "Tobias";
+    document.getElementById('name_chillen').innerHTML = "Jonathan";
+}
+
+if(currentMode === 2) {
+    document.getElementById('name_staubsaugen').innerHTML = "Tobias";
+    document.getElementById('name_badezimmer').innerHTML = "Jonathan";
+    document.getElementById('name_chillen').innerHTML = "Tobia";
+}
+
+}
+
+let oneDay = 24 * 60 * 60 * 1000;
+setInterval(getAemtli(),oneDay);
