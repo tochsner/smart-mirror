f0c8109beaae35280c0faf75d803fffe9c2134a1
Tobia Ochsner <tobia.ochnser@hotmail.com>
2022-01-04 21:54:20 +0100

    Implement train connection queries.

--- src/sbb.js
+++ src/sbb.js
@@ -1,8 +1,47 @@
-async function getSbbToSh() {
-url = 'https://google.com';
-const res = await axios.get(url);
-let page = res.data;
-console.log(page);
+function formatTimeString(timeString) {
+  const date = new Date(Date.parse(timeString));
+
+  var formatOptions = { hour: "2-digit", minute: "2-digit" };
+  return date.toLocaleString("de-CH", formatOptions);
 }
 
-getSbbToSh();
+function getDelay(planned, predicted) {
+  const msPlanned = Date.parse(planned);
+  const msPredicted = Date.parse(predicted);
+
+  const delay = Math.round((msPredicted - msPlanned) / 1000 / 60);
+
+  if (delay > 0) return `+${delay}`;
+  else return `${delay}`;
+}
+
+async function getSbbToSh(destination) {
+  const url = `http://transport.opendata.ch/v1/connections?from=Dübendorf&to=${destination}`;
+  const response = await axios.get(url);
+
+  const nextConnection = response.data.connections[0];
+
+  const plannedDeparture = nextConnection.from.departure;
+  const predictedDeparture = nextConnection.from.prognosis.departure;
+
+  const plannedArrival = nextConnection.to.arrival;
+
+  document.getElementById(`${destination}-dep`).innerHTML =
+    formatTimeString(plannedDeparture);
+  document.getElementById(`${destination}-arr`).innerHTML =
+    formatTimeString(plannedArrival);
+
+  if (plannedDeparture === predictedDeparture) {
+    document.getElementById(`${destination}-delay`).style.display = "none";
+  } else {
+    document.getElementById(`${destination}-delay`).style.display = "visible";
+    document.getElementById(`${destination}-delay`).innerHTML = getDelay(
+      plannedDeparture,
+      predictedDeparture
+    );
+  }
+}
+
+getSbbToSh("schaffhausen");
+getSbbToSh("hb");
+getSbbToSh("eth");
