c170fea51db0faae606cddf10b303c4bf7325a5a
Jonathan Dotzler <63780364+jonarivado@users.noreply.github.com>
2025-12-07 17:36:07 +0100

    add rafi

--- src/aemtli.js
+++ src/aemtli.js
@@ -7,21 +7,21 @@ function getAemtli() {
   const currentMode = (weeksSinceStart - 1) % 3;
 
   if (currentMode === 0) {
-    document.getElementById("name_staubsaugen").innerHTML = "Jonathan/Nadina";
-    document.getElementById("name_badezimmer").innerHTML = "Annik";
+    document.getElementById("name_staubsaugen").innerHTML = "Jonathan";
+    document.getElementById("name_badezimmer").innerHTML = "Rafi";
     document.getElementById("name_chillen").innerHTML = "Tobias";
   }
 
   if (currentMode === 1) {
-    document.getElementById("name_staubsaugen").innerHTML = "Annik";
+    document.getElementById("name_staubsaugen").innerHTML = "Rafi";
     document.getElementById("name_badezimmer").innerHTML = "Tobias";
-    document.getElementById("name_chillen").innerHTML = "Jonathan/Nadina";
+    document.getElementById("name_chillen").innerHTML = "Jonathan";
   }
 
   if (currentMode === 2) {
     document.getElementById("name_staubsaugen").innerHTML = "Tobias";
-    document.getElementById("name_badezimmer").innerHTML = "Jonathan/Nadina";
-    document.getElementById("name_chillen").innerHTML = "Annik";
+    document.getElementById("name_badezimmer").innerHTML = "Jonathan";
+    document.getElementById("name_chillen").innerHTML = "Rafi";
   }
 }
 
